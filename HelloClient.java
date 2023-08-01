3813496dd20446c639ae0e87fea09f17c63e4333
Basavaraj-S-B <113363280+Basavaraj-S-B@users.noreply.github.com>
2023-08-01 12:11:25 +0530

    HelloClient.java

---
+++ HelloClient.java
@@ -0,0 +1,16 @@
+package rmi;
+import java.rmi.*;
+import java.io.*;
+
+public class HelloClient {
+     
+	public static void main(String[] argv) {
+		try {
+			HelloInterface hello = (HelloInterface) Naming.lookup("//localhost/Hello");
+			System.out.println(hello.say());
+		}
+		catch(Exception e) {
+			System.out.println("Hello client Exception " + e);
+		}
+	}
+}
